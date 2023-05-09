0e8a60f002bfbe2e123c7bfaa9460a82f850642e
Ren Koya <112790933+RenKoya1@users.noreply.github.com>
2023-05-10 00:44:56 +0400

    Update blackToWhite.py

--- blackToWhite.py
+++ blackToWhite.py
@@ -2,8 +2,8 @@ import cv2
 import numpy as np
 
 def change_background(src_path, dst_path):
-    img = cv2.imread(src_path, cv2.IMREAD_UNCHANGED)  # アルファチャンネルを含めて読み込む
-    alpha = img[:, :, 3]
+    img = cv2.imread(src_path, cv2.IMREAD_UNCHANGED) 
+    alpha = img[:, :, 3]   # alpha channel
     img_rgb = img[:, :, :3]
     gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
     ret, binary = cv2.threshold(gray, 2, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
