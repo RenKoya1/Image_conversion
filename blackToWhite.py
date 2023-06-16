628f34377957b6bcd444a1eb4156bbe604a0fca1
RenKoya1 <koya.ren.701@gmail.com>
2023-06-16 19:05:20 +0400

    FIX variable

--- blackToWhite.py
+++ blackToWhite.py
@@ -2,17 +2,15 @@ import cv2
 import numpy as np
 
 def black_to_white(src_path, dst_path):
-    img = cv2.imread(src_path, cv2.IMREAD_UNCHANGED) 
+    img = cv2.imread(src_path, cv2.IMREAD_UNCHANGED)
     alpha = img[:, :, 3]   # alpha channel
     img_rgb = img[:, :, :3]
-    gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
-    ret, binary = cv2.threshold(gray, 2, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
     
-    img_rgb[binary == 255] = 255
+    img_rgb[(img_rgb <= [50,50,50]).all(axis=2)] = [255,255,255]
 
     result_with_alpha = np.concatenate([img_rgb, alpha[..., None]], axis=2)
     result_with_alpha[alpha == 0] = [0, 0, 0, 0]
 
     cv2.imwrite(dst_path, result_with_alpha)
 
-black_to_white("SUP.png", "result.png")
+black_to_white("Pmeta.png", "result.png")
